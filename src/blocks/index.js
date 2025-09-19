c171a141e9e8fb8d260b0a846f2efae1e92297a5
AwasthiNikhil <nikhilawasthi944@gmail.com>
2025-09-19 07:01:40 +0545

    function registerCoreBlocks()

--- src/blocks/index.js
+++ src/blocks/index.js
@@ -1,25 +1,16 @@
-// import { registerBlockType } from "@wordpress/blocks";
-// import * as paragraph from "./paragraph";
-
-// const blocks = [paragraph];
-
-// export const registerAllBlocks = () => {
-//   blocks.forEach(({ name, settings }) => {
-//     registerBlockType(name, settings);
-//   });
-// };
-
 import { registerBlockType } from "@wordpress/blocks";
+import { registerCoreBlocks } from '@wordpress/block-library';
 
 const requireBlock = require.context(".", true, /index\.js$/);
 
 export const registerAllBlocks = () => {
-  requireBlock.keys().forEach((filename) => {
-    const blockModule = requireBlock(filename);
-    const { name, settings } = blockModule;
+  // requireBlock.keys().forEach((filename) => {
+  //   const blockModule = requireBlock(filename);
+  //   const { name, settings } = blockModule;
 
-    if (name && settings) {
-      registerBlockType(name, settings);
-    }
-  });
+  //   if (name && settings) {
+  //     registerBlockType(name, settings);
+  //   }
+  // });
+  registerCoreBlocks();
 };
